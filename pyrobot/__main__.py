a2af398594c97886e8c27454f7a4fd9834c8ddf5
drsyed1 <69309485+drsyed1@users.noreply.github.com>
2020-12-26 23:15:14 +0530

    Update __main__.py

--- pyrobot/__main__.py
+++ pyrobot/__main__.py
@@ -1,24 +1,3 @@
-#  Copyright (c) 2019-2020 Dan <https://github.com/delivrance>
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a
-#  copy of this software and associated documentation files (the "Software"),
-#  to deal in the Software without restriction, including without
-#  limitation the rights to use, copy, modify, merge, publish,
-#  distribute, sublicense, and/or sell copies of the Software,
-#  and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be
-#  included in all copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-#  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-#  OTHER DEALINGS IN THE SOFTWARE.
-
 from .pyrobot import PyroBot
 
 import os
@@ -32,7 +11,7 @@ user_session_string = "BQCQX5yd3ipFkANBGiquKBfn851U9-i_rl9By2pDVU59RdgBayy8-sX5a
 bots = ["WhiteEyeRenameBot"]
 bot_owner = ("@Mr_StarLords")
 update_channel = (-1001484903966)
-status_message_id = (215)
+status_message_id = (926862796)
 api_id = ("2412622")
 api_hash = ("fa25ca6c7d6723ea717eee08e6af6565")
 
