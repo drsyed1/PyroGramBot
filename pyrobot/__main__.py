e1821ad2f63d83521d09371aad343a043ad4e909
drsyed1 <69309485+drsyed1@users.noreply.github.com>
2020-12-26 15:03:33 +0530

    Update __main__.py

--- pyrobot/__main__.py
+++ pyrobot/__main__.py
@@ -28,19 +28,18 @@ import datetime
 import pyrogram
     
     
-user_session_string = os.environ.get("1BVtsOHwBuwqrsinlWxs5au0hdAmI3kcYtpy5BK97xaZda6EDkiEIA90r0bnjM6A6yVjxfBRKDfzkifyOSIccS5MqoiCNPD_3cb1b8Ox4htuyKpOtSZpbtxyO6iM2k73uUMEBzd4f1eu9mqufc1nH2MkMjo_F4XAGTnyNwZ1U876CX9yYTr0sM0bRMCQ9nz8U_00wJwbINrCtTJwhckVqiVEzy7FjmKF2swH5wJw89VgXDGjZ53kC6j2GceYeuMhdXr1xsM557XWjznTWsvVUoKG4MF2jM0XYwOwnfJmqLi5Z3uUUggXnT2OGjvi9wjB-P-dYne5JbYMcxmRP28mq9oP4DDOYmso=")
-bots = os.environ.get('@WhiteEyeRenameBot'), ('@WhiteEyeUrlUploaderBot'), ('@WhiteEyeYouTubeBot'), ('@WhiteEyeDeleteAllBot'), ('@WhiteEyeCompressorBot'), ('@Miss_ArantxaBot'), ('@WhiteEyeSubtitleBot'), ('@WhiteEyeLinkToFileBot'), ('@WhiteEyeGDriveBot'), ('@WhiteEyeTelegraphBot'), ('@WhiteEyeForceSubscriberBot'), ('@WhiteEyeUltraTonBot'), ('@WhiteEyeTagRemoverBot')
-bot_owner = os.environ.get("@Mr_StarLords")
-update_channel = os.environ.get("-1001484903966")
-api_id = (os.environ.get("1715074"))
-api_hash = os.environ.get("0c8fb6a43409019900aa98f439eceec4")
+user_session_string = "1BVtsOHwBuwqrsinlWxs5au0hdAmI3kcYtpy5BK97xaZda6EDkiEIA90r0bnjM6A6yVjxfBRKDfzkifyOSIccS5MqoiCNPD_3cb1b8Ox4htuyKpOtSZpbtxyO6iM2k73uUMEBzd4f1eu9mqufc1nH2MkMjo_F4XAGTnyNwZ1U876CX9yYTr0sM0bRMCQ9nz8U_00wJwbINrCtTJwhckVqiVEzy7FjmKF2swH5wJw89VgXDGjZ53kC6j2GceYeuMhdXr1xsM557XWjznTWsvVUoKG4MF2jM0XYwOwnfJmqLi5Z3uUUggXnT2OGjvi9wjB-P-dYne5JbYMcxmRP28mq9oP4DDOYmso="
+bots = ["@WhiteEyeRenameBot", "@bot2"]
+bot_owner = ("@Mr_StarLords")
+update_channel = ("-1001484903966")
+api_id = ("1715074")
+api_hash = ("0c8fb6a43409019900aa98f439eceec4")
 
 user_client = pyrogram.Client(
     user_session_string, api_id=api_id, api_hash=api_hash)
 
 
 def main():
-    with user_client:
         while True:
             print("[INFO] starting to check uptime..")
             edit_text = f"@{update_channel} Bot's Uptime Status.(Updated every 15 mins)\n\n"
@@ -73,4 +72,4 @@ def main():
             time.sleep(15 * 60)
 
 
-main()    
+main() 
